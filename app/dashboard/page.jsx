5f84578a959a30f26dfb931bb1ee07c3c4ad3e81
Akshat <aroraakshat1603@gmail.com>
2025-07-07 02:12:57 +0530

    display courses on dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import WelcomeBanner from "./_components/WelcomeBanner";
-//import CourseList from "./_components/CourseList";
+import CourseList from "./_components/CourseList";
 
 {/*This file is specific to the route and dynamically changes based on the user, data, or interactions.*/}
 function Dashboard() {
   return (
     <div>
       <WelcomeBanner />
-      {/* <CourseList /> */}
+      <CourseList />
     </div>
   );
 }
